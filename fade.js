62ea64a1af3a13654de7554a52641322fdefbb23
jangju <wkdwndud13@naver.com>
2020-04-13 22:34:59 +0900

    2020-04-13

--- fade.js
+++ fade.js
@@ -35,19 +35,33 @@ $(document).ready(function () {
         });
     });
 });
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.article_project').each(function (i) {
+
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_element) {
+                $(this).animate({ 'opacity': '1', 'margin': '100px' }, 1000);
+            }
+
+        });
+    });
+});
 
 function fnMove(num) {//원하는 좌표로 이동
     var offset = $(num).offset();
     $('html, body').animate({ scrollTop: offset.top }, 900);
 }
 
-$(window).scroll(function () {
+$(window).scroll(function () {//ABOUT 
     if ($(window).scrollTop() < 600) {
         $('#HOME_NAV').addClass('scroll_top');
     }
 })
 
-$(window).scroll(function(){
+$(window).scroll(function(){//SKILLS
     if($(window).scrollTop()>600){
         $('#ABOUT_NAV').addClass('scroll_top');
         $('#HOME_NAV').removeClass('scroll_top');
@@ -66,9 +80,17 @@ $(window).scroll(function () {
 })
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() > 1500) {
-        $('#CONTANT_NAV').addClass('scroll_top');
+    if ($(window).scrollTop() > 1750) {
+        $('#PROJECT_NAV').addClass('scroll_top');
         $('#SKILLS_NAV').removeClass('scroll_top');
+    } else {
+        $('#PROJECT_NAV').removeClass('scroll_top');
+    }
+})
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 2200) {
+        $('#CONTANT_NAV').addClass('scroll_top');
+        $('#PROJECT_NAV').removeClass('scroll_top');
     } else {
         $('#CONTANT_NAV').removeClass('scroll_top');
     }
---
+++ index.js
@@ -0,0 +1,51 @@
+function github_on(e) {
+    window.open("https://github.com/jangju/Jang");
+}
+
+var modal_on = false;
+var modal_youtube_on = false;
+var modal = document.getElementsByClassName("modal");
+
+/* Youtube Api*/
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+/* Youtube Api end*/
+
+function youtube_on(e) {
+    if (modal_on === false) {
+        modal_on = true;
+        modal[0].style.display = "flex";
+    }
+    if (e.length > 0) {
+        this.youtube_link = e + "";
+    }
+    player = new YT.Player("player", {
+        height: "1080",
+        width: "1920",
+        videoId: e,
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange
+        }
+    });
+    function onPlayerReady(event) {
+        event.target.setVolume(10);
+        event.target.setPlaybackQuality("hd720");
+        event.target.playVideo();
+    }
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.BUFFERING) {
+            event.target.setPlaybackQuality("hd720"); // <-- WORKS!
+        }
+    }
+}
+function modal_off() {
+    if (modal_on === true) {
+        modal[0].style.display = "none";
+        modal_on = false;
+    }
+    player.destroy(); // 유튜브 객체 삭제(페이지 렉 방지)
+}
